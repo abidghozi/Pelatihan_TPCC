43cd38ea728d6f2e126ebcbb049715ef51ccb032
abidghozi <abidghozi.g@gmail.com>
2016-11-04 13:03:03 +0700

    Nambah Log-in(No Func)

--- app/src/main/java/com/example/gigabyte/mainproject/MainApp.java
+++ app/src/main/java/com/example/gigabyte/mainproject/MainApp.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainApp extends AppCompatActivity {
 
@@ -12,6 +14,8 @@ public class MainApp extends AppCompatActivity {
     private Button button_app4;
     private Button button_app2;
     private Button button_app1;
+    private ImageView bImage;
+    private TextView txtuser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,11 @@ public class MainApp extends AppCompatActivity {
         button_app4 = (Button) findViewById(R.id.bApp4);
         button_app2 = (Button) findViewById(R.id.bApp2);
         button_app1 = (Button) findViewById(R.id.bApp1);
+        bImage = (ImageView) findViewById(R.id.imageView2);
+        txtuser = (TextView) findViewById(R.id.textView10);
+
+        DatabaseHandler db = new DatabaseHandler(this);
+        System.out.println("USER : "+db.getUserCount());
 
         //Intent Function
         button_app4.setOnClickListener(new View.OnClickListener() {
@@ -37,6 +46,22 @@ public class MainApp extends AppCompatActivity {
             public void onClick(View view) {
                 Intent app2 = new Intent(MainApp.this, Quiz.class);
                 startActivity(app2);
+            }
+        });
+
+        button_app1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent app1 = new Intent(MainApp.this, Counter.class);
+                startActivity(app1);
+            }
+        });
+
+        bImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sh = new Intent(MainApp.this, NewUser.class);
+                startActivity(sh);
             }
         });
     }
